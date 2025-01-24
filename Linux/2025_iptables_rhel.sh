b2e435bbfcfeefc5f409dd1b6b1d9d748c08e67e
rhit-phunga <116199012+rhit-phunga@users.noreply.github.com>
2025-01-24 12:23:03 -0500

    Update and rename 2025_fedora_iptables.sh to 2025_iptables_rhel.sh

--- Linux/2025_iptables_rhel.sh
+++ Linux/2025_iptables_rhel.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 # If possible, ask judge about changing smpt and pop3 port to more secure version
+echo "Be sure to check if the port policy on the script pertains to your use"
+iptables -F
+iptables -P INPUT DROP
+
 iptables -A INPUT -p tcp --dport 25 -j ACCEPT
 iptables -A INPUT -p tcp --dport 110 -j ACCEPT
-
+# iptables -A INPUT -p tcp --dport 80 -j ACCEPT
 # ==
 iptables -N LOG_AND_DROP
 iptables -A LOG_AND_DROP -j LOG --log-prefix "Port Scan: " --log-level 4
@@ -16,3 +20,5 @@ iptables -A INPUT -p tcp --tcp-flags FIN,RST FIN,RST -j DROP
 iptables -A INPUT -p tcp --tcp-flags ALL ALL -j DROP
 iptables -A INPUT -p tcp --tcp-flags ALL NONE -j LOG --log-prefix "NULL Scan: "
 iptables -A INPUT -p tcp --tcp-flags ALL ALL -j LOG --log-prefix "XMAS Scan: "
+
+echo "DONE"
